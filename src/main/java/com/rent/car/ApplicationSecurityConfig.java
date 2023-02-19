5708cb217bf4fd45a7a111ac5cd4c4d3b8f3c01e
Petar-Ivanov <stu2001261009@uni-plovdiv.bg>
2023-02-19 21:31:49 +0200

    CSRF and CSP added

--- src/main/java/com/rent/car/ApplicationSecurityConfig.java
+++ src/main/java/com/rent/car/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.rent.car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -39,19 +40,63 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http
-		.csrf().disable()
-		.authorizeRequests()
-		.antMatchers("/login", "/register", "/assets/**", "/actors/addNew").permitAll()
-		.anyRequest().authenticated()
-		.and()
-		.formLogin()
-		.loginPage("/login").permitAll()
-		.defaultSuccessUrl("/index")
-		.and().exceptionHandling().accessDeniedPage("/accessDenied")
-		.and()
-		.logout().invalidateHttpSession(true)
-		.clearAuthentication(true)
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-		.logoutSuccessUrl("/login").permitAll();
+			.csrf()
+				.requireCsrfProtectionMatcher(new AntPathRequestMatcher("/login", HttpMethod.POST.name()))
+				.requireCsrfProtectionMatcher(new AntPathRequestMatcher("/actors/addNew", HttpMethod.POST.name()))
+				.and()
+			.headers()
+				.xssProtection()
+					.and()
+				.contentSecurityPolicy("form-action 'self'").and()
+				//.contentSecurityPolicy(getContentSecurityPolicy()).and()
+				.frameOptions().sameOrigin()
+					.and()
+			.authorizeRequests()
+				.antMatchers("/login", "/assets/**", "/register", "/actors/addNew").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login").permitAll()
+				.defaultSuccessUrl("/index")
+				.and()
+			.exceptionHandling()
+				.accessDeniedPage("/accessDenied")
+				.and()
+			.logout()
+				.invalidateHttpSession(true)
+				.clearAuthentication(true)
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/login").permitAll();
 	}
+	
+//	private String getContentSecurityPolicy() {
+//		
+//		String defaultSrc = "default-src 'self'";
+//		String styleSrc = "style-src 'self' fonts.googleapis.com";
+//		String fontSrc = "font-src 'self' fonts.gstatic.com 'nonce-" + getNonce() + "'";
+//		String scriptSrc = "script-src 'self' "
+//						 + "https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js "
+//						 + "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js "
+//						 + "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js "
+//						 + "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js "
+//						 + "https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js";
+//		String formAction = "form-action 'self'";
+//		
+//		return  defaultSrc + "; " +
+//				styleSrc + "; " +
+//				fontSrc + "; " +
+//				scriptSrc + "; " +
+//				formAction;
+//	}
+//	
+//	private String getNonce() {
+//		
+//		final int NONCE_LENGTH = 16;
+//		
+//		SecureRandom random = new SecureRandom();
+//		byte[] nonceBytes = new byte[NONCE_LENGTH];
+//		random.nextBytes(nonceBytes);
+//		
+//		return Base64.getEncoder().encodeToString(nonceBytes);
+//	}
 }
